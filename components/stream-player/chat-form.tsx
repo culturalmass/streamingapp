b573365219f47aae0ee24f65d555334e530b3225
culturalmass <herrera_manuel18@hotmail.com>
2024-02-02 13:05:52 +0100

    Update Code 15

--- components/stream-player/chat-form.tsx
+++ components/stream-player/chat-form.tsx
@@ -5,6 +5,7 @@ import { cn } from "@/lib/utils";
 import { Input } from "../ui/input";
 import { Button } from "../ui/button";
 import { Skeleton } from "../ui/skeleton";
+import { ChatInfo } from "./chat-info";
 
 interface ChatFormProps {
   onSubmit: () => void;
@@ -19,10 +20,73 @@ interface ChatFormProps {
 export const ChatForm = ({
   onSubmit,
   value,
+  onChange,
   isHidden,
   isFollowersOnly,
   isFollowing,
   isDelayed,
 }: ChatFormProps) => {
-  return <div>chatform</div>;
+  const [isDelayBloqued, setIsDelayBlocked] = useState(false);
+
+  const isFollowersOnlyAndNotFollowing = isFollowersOnly && !isFollowing;
+  const isDisabled =
+    isHidden || isDelayBloqued || isFollowersOnlyAndNotFollowing;
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    e.stopPropagation();
+
+    if (!value || isDisabled) return;
+
+    if (isDelayed && !isDelayBloqued) {
+      setIsDelayBlocked(true);
+      setTimeout(() => {
+        setIsDelayBlocked(false);
+        onSubmit();
+      }, 3000);
+    } else {
+      onSubmit();
+    }
+  };
+
+  if (isHidden) {
+    return null;
+  }
+  return (
+    <form
+      onSubmit={handleSubmit}
+      className="flex flex-col items-center gap-y-4 p-3"
+    >
+      <div className="w-full">
+        <ChatInfo isDelayed={isDelayed} isFollowersOnly={isFollowersOnly} />
+        <Input
+          onChange={(e) => onChange(e.target.value)}
+          value={value}
+          disabled={isDisabled}
+          placeholder="Send a message"
+          className={cn(
+            "border-white/10",
+            isFollowersOnly && "rounded-t-none border-t-0"
+          )}
+        />
+      </div>
+      <div className="ml-auto">
+        <Button type="submit" variant="primary" size="sm" disabled={isDisabled}>
+          Chat
+        </Button>
+      </div>
+    </form>
+  );
+};
+
+export const ChatFormSkeleton = () => {
+  return (
+    <div className="flex flex-col items-center gap-y-4 p-3">
+      <Skeleton className="w-full h-10" />
+      <div className="flex items-center gap-x-2 ml-auto">
+        <Skeleton className="h-7 w-7" />
+        <Skeleton className="h-7 w-7" />
+      </div>
+    </div>
+  );
 };
--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,21 @@
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { type ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
 }
+
+export const stringToColor = (str: string) => {
+  let hash = 0;
+  // Loop through each character of the string
+  for (let i = 0; i < str.length; i++) {
+    // Add the ASCII value of the character to the hash
+    hash = hash + str.charCodeAt(i);
+  }
+  // Convert the hash to a hexadecimal number
+  let hex = hash.toString(16);
+  // Pad the hex with zeros if needed
+  hex = hex.padStart(6, "0");
+  // Return the hex color with a # prefix
+  return "#" + hex;
+};
